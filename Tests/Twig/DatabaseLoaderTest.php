d0fb662b7c36d3b651fe429fd223cd73d76c30b4
Vyacheslav Enis <venis@difane.com>
2012-08-14 01:54:43 +0300

    Created initial dummy PHPUnit test.

---
+++ Tests/Twig/DatabaseLoaderTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Difane\Bundle\TwigDatabaseBundle\Tests\Twig;
+
+class DatabaseLoaderTest extends \PHPUnit_Framework_TestCase
+{
+    public function testDummy()
+    {
+        $this->assertTrue(true);
+    }
+}
